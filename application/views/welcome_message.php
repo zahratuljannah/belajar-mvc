2a1d21d30847e76617c1e334dbc210e57a6c64b8
Bang Damar <46801556+damaryosaaji@users.noreply.github.com>
2023-01-12 10:15:29 +0700

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -83,19 +83,24 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
 		<div id="body">
-			<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+			<h2>INI SIT</h2>
+			<p>
+			<h2>INI SIT 2</h2> The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 			<p>If you would like to edit this page you'll find it located at:</p>
 			<code>application/views/welcome_message.php</code>
 
 			<p>The corresponding controller for this page is found at:</p>
-			<code>application/controllers/Welcome.php</code>
+			<code>
+				<h2>INI SIT 3</h2> application/controllers/Welcome.php
+			</code>
 
 			<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 		</div>
 
 		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 	</div>
+	<h2>INI SIT END</h2>
 	<div> Ini dari SIT HOTFIX</div>
 </body>
 
