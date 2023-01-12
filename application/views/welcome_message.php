6d769d253c0483ff9ef5be97ea009779bda5de0a
zahratuljannah <zahratuljannah358901@gmail.com>
2023-01-12 09:46:15 +0700

    add paragraf 1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -70,6 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
 	<h1>hello world</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta maiores cupiditate magnam, a dignissimos dicta eum beatae pariatur praesentium similique.</p>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
@@ -80,7 +81,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">zahratul jannah</a>.</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
